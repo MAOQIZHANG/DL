496c7479855daffaf30bb1646df566c684e64d48
MAOQIZHANG <67251502+MAOQIZHANG@users.noreply.github.com>
2023-05-01 12:15:28 -0400

    change to 5000

--- nextframe_model.py
+++ nextframe_model.py
@@ -191,7 +191,7 @@ if __name__ == "__main__":
   valid_root_dirs=['Dataset_Student/val']
 
 
-  batch_size = 32
+  batch_size = 64  #change for time
   epochs = 100
   learning_rate = 0.001
   device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -199,8 +199,8 @@ if __name__ == "__main__":
 
   # Create dataset and dataloader
   #number could be added here 
-  train_dataset = VideoDataset(train_root_dirs)
-  val_dataset = VideoDataset(valid_root_dirs)
+  train_dataset = VideoDataset(train_root_dirs, 5000)
+  val_dataset = VideoDataset(valid_root_dirs, 500)
   train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
   val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
 
