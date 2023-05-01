bcde31165f8ab618a76c24fce4e9d3d99336ddf9
MAOQIZHANG <67251502+MAOQIZHANG@users.noreply.github.com>
2023-05-01 03:34:36 -0400

    change to full dataset

--- nextframe_model.py
+++ nextframe_model.py
@@ -198,8 +198,8 @@ if __name__ == "__main__":
 
   # Create dataset and dataloader
   #number could be added here 
-  train_dataset = VideoDataset(train_root_dirs, 125)
-  val_dataset = VideoDataset(valid_root_dirs, 25)
+  train_dataset = VideoDataset(train_root_dirs)
+  val_dataset = VideoDataset(valid_root_dirs)
   train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
   val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
 
