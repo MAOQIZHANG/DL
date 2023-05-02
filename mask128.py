97c8feaf3d808643ff8bc82ed90687be7521a4dd
MAOQIZHANG <67251502+MAOQIZHANG@users.noreply.github.com>
2023-05-02 17:31:34 -0400

    add predict

--- mask128.py
+++ mask128.py
@@ -24,12 +24,7 @@ for filename in os.listdir(dir_path):
     if os.path.isfile(os.path.join(dir_path, filename)):
         file_, ext = os.path.splitext(filename)
         tensor = torch.load(os.path.join(dir_path, filename))
-        numpy_array = tensor.numpy()
-        image = Image.fromarray(numpy_array)
-        tensor = torch.from_numpy(numpy_array)
-
         print('load success')
-        image = image.unsqueeze(0)  # Add a batch dimension
         image = image.to(device)  # Send the image to the device
         with torch.no_grad():
             output = model(image)
@@ -39,6 +34,30 @@ for filename in os.listdir(dir_path):
         mask = np.squeeze(mask)
         output_path = os.path.join(output_folder, file_)+".npy"
         np.save(output_path, mask)
+# Specify the directory
+dir_path = 'mask_128'
+
+# Initialize an empty list to hold the numpy arrays
+arrays = []
+
+# Loop over the file numbers
+for i in range(15000, 17000):  # Modify this range as needed
+    # Construct the file name
+    file_name = f'22nd_frame_video_{i}.npy'
+    file_path = os.path.join(dir_path, file_name)
+
+    # Check if the file exists
+    if os.path.isfile(file_path):
+        # Load the numpy array from the file
+        array = np.load(file_path)
+        # Append the array to the list
+        arrays.append(array)
+
+# Stack the arrays along the first dimension (axis=0)
+stacked_array = np.stack(arrays, axis=0)
+
+# Save the stacked array to a file
+np.save('final/final_128.npy', stacked_array)
 
 print('finish')
 '''
